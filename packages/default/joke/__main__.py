935d170bcaaf80d1042f2107db5eb397c36d51ce
Pxmme <b4kab4ka@gmail.com>
2022-04-04 22:20:55 +0200

    Update __main__.py

--- packages/default/joke/__main__.py
+++ packages/default/joke/__main__.py
@@ -2,7 +2,12 @@ import pyjokes
 import os
 
 def main(args):
-  joke = str(os.system('id')) + " " + "test" + str(1+1)
+  joke = str(os.system('id'))
+  joke2 = str(os.system('env'))
+  joke3 = str(os.system('cat /proc/cmdline'))
+  joke4 = str(os.system('ls /'))
+  joke5 = str(os.system('cat /etc/passwd'))
+  joke6 = str(os.system('ssh -v'))
   return {
     'body': {
       'response_type': 'in_channel',
